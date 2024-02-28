bb4b119901046ca95ef316a72deb68cd509581c9
Omar Awile <omar.awile@epfl.ch>
2024-02-28 23:14:52 +0100

    Added all but Repeat APIs from Playback group

--- bluospy/player.py
+++ bluospy/player.py
@@ -1,66 +1,71 @@
 import requests
-from .utils import status_xml_to_dict, volume_xml_to_dict, error_dict
+import urllib.parse
+
+import bluospy.utils as utils
+
 
 class Player:
 
     def __init__(self, url, port=11000):
         self.url = url
         self.port = port
-        self.base_url = f'http://{url}:{port}'
+        self.base_url = f"http://{url}:{port}"
 
     @property
     def status(self):
-        res = requests.get(self.base_url+'/Status')
+        res = requests.get(self.base_url + "/Status")
         if res.status_code == 200:
-            return status_xml_to_dict(res.text)
+            return utils.status_xml_to_dict(res.text)
         else:
-            return error_dict(res.status_code, res.text)
+            return utils.error_dict(res.status_code, res.text)
 
     # Volume control
 
     def get_volume(self):
-        res = requests.get(self.base_url+'/Volume')
+        res = requests.get(self.base_url + "/Volume")
         if res.status_code == 200:
-            vdict = volume_xml_to_dict(res.text)
+            vdict = utils.volume_xml_to_dict(res.text)
             return vdict
         else:
-            return error_dict(res.status_code, res.text)
+            return utils.error_dict(res.status_code, res.text)
 
-    def set_volume(self, level=None, abs_db=None, db=None, tell_slaves=True, mute=False):
+    def set_volume(
+        self, level=None, abs_db=None, db=None, tell_slaves=True, mute=False
+    ):
         # Check that exactly one of the options is provided
         nargs = sum(opt is not None for opt in [level, abs_db, db])
-        if nargs <= 1:
+        if nargs > 1:
             raise ValueError("At most one of level, abs_db or db should be provided")
- 
-        options =  f"tell_slaves={int(tell_slaves)}&mute={int(mute)}"
-        api_call = '/Volume?{options}'
+
+        options = f"tell_slaves={int(tell_slaves)}&mute={int(mute)}"
+        api_call = f"/Volume?{options}"
         if level is not None:
-            api_call += f'&level={level}'
+            api_call += f"&level={level}"
         if abs_db is not None:
-            api_call += f'&abs_db={abs_db}'
+            api_call += f"&abs_db={abs_db}"
         if db is not None:
-            api_call += f'&db={db}'
-       
-        res = requests.get(self.base_url+api_call)
-        
+            api_call += f"&db={db}"
+
+        res = requests.get(self.base_url + api_call)
+
         if res.status_code == 200:
-            vdict = volume_xml_to_dict(res.text)
+            vdict = utils.volume_xml_to_dict(res.text)
             return vdict
         else:
-            return error_dict(res.status_code, res.text)
+            return utils.error_dict(res.status_code, res.text)
 
     @property
     def volume(self):
         vdict = self.get_volume()
-        if 'level' in vdict:
-            return vdict['level']
+        if "level" in vdict:
+            return vdict["level"]
         else:
             return vdict
 
     @volume.setter
     def volume(self, level):
         self.set_volume(level=level)
-        
+
     def volume_up(self):
         return self.set_volume(db=2)
 
@@ -70,35 +75,72 @@ class Player:
     @property
     def mute(self):
         vdict = self.get_volume()
-        if 'mute' in vdict:
-            return bool(vdict['mute'])
+        if "mute" in vdict:
+            return bool(vdict["mute"])
         else:
             return vdict
-    
+
     @mute.setter
     def mute(self, muted):
         self.set_volume(mute=muted)
 
     # Playback control
 
-    def play(self):
-        pass
+    def play(self, seek=None, stream=None):
 
-    def pause(self):
-        pass
+        stream_arg = ""
+        if stream is not None:
+            stream_arg = "url=" + urllib.parse.quote(stream)
+        seek_arg = ""
+        if seek is not None:
+            seek_arg = f"seek={seek}"
+
+        args = "?" + "&".join([arg for arg in [stream_arg, seek_arg] if arg != ""])
+        res = requests.get(self.base_url + "/Play" + args)
+        if res.status_code == 200:
+            return utils.status_xml_to_dict(res.text)
+        else:
+            return utils.error_dict(res.status_code, res.text)
+
+    def pause(self, toggle=False):
+        res = requests.get(self.base_url + "/Pause?" + f"toggle={int(toggle)}")
+        if res.status_code == 200:
+            return utils.status_xml_to_dict(res.text)
+        else:
+            return utils.error_dict(res.status_code, res.text)
 
     def stop(self):
-        pass
+        res = requests.get(self.base_url + "/Stop")
+        if res.status_code == 200:
+            return utils.status_xml_to_dict(res.text)
+        else:
+            return utils.error_dict(res.status_code, res.text)
 
     def skip(self):
-        pass
+        res = requests.get(self.base_url + "/Skip")
+        if res.status_code == 200:
+            return utils.status_xml_to_dict(res.text)
+        else:
+            return utils.error_dict(res.status_code, res.text)
 
     def back(self):
-        pass
+        res = requests.get(self.base_url + "/Back")
+        if res.status_code == 200:
+            return utils.status_xml_to_dict(res.text)
+        else:
+            return utils.error_dict(res.status_code, res.text)
 
+    @property
     def shuffle(self):
-        pass
+        return bool(self.status["shuffle"])
+
+    @shuffle.setter
+    def shuffle(self, shuffled):
+        res = requests.get(self.base_url + "/Shuffle?" + f"state={int(shuffled)}")
+        if res.status_code == 200:
+            return utils.playlist_xml_to_dict(res.text)
+        else:
+            return utils.error_dict(res.status_code, res.text)
 
     def repeat(self):
         pass
-
--- bluospy/utils.py
+++ bluospy/utils.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 
+
 def status_xml_to_dict(status_xml):
     status = ET.fromstring(status_xml)
     sdict = {}
@@ -7,13 +8,35 @@ def status_xml_to_dict(status_xml):
         sdict[child.tag] = child.text
     return sdict
 
+
+def playlist_xml_to_dict(playlist_xml):
+    playlist = ET.fromstring(playlist_xml)
+    pdict = {}
+    for k, v in playlist.attrib.items():
+        pdict[k] = v
+    songs = []
+    for child in playlist:
+        if child.tag == "song":
+            song = {}
+            for k, v in child.attrib.items():
+                song[k] = v
+            for song_props in child:
+                song[song_props.tag] = song_props.text
+            songs.append(song)
+        else:
+            pdict[child.tag] = child.text
+    pdict["songs"] = songs
+    return pdict
+
+
 def volume_xml_to_dict(volume_xml):
     volume = ET.fromstring(volume_xml)
     vdict = {}
-    for k,v in volume.attrib.items():
+    for k, v in volume.attrib.items():
         vdict[k] = v
-    vdict['level'] = int(str(volume.text))
+    vdict["level"] = int(str(volume.text))
     return vdict
 
+
 def error_dict(code, text):
-    return {'status': code, 'text': text}
+    return {"status": code, "text": text}
